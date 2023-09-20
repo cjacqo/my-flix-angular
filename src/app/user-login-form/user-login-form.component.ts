c8a38025f28d26bccf329c9753a80ab8c3c47317
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-09-19 17:16:28 -0700

    UserRegistrationForm and UserLoginForm are built with material design, user inputs with two way data binding and visible through the apps root component

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent {
+  @Input() userData = { UserName: '', Password: '' }
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar
+  ) { }
+
+  ngOnInit(): void { }
+
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe(result => {
+      this.dialogRef.close()
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      })
+    }, result => {
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      })
+    })
+  }
+}
