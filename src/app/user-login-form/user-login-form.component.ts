812db492dcc2fc80e9119e68f02bc64296caead0
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-09-20 19:57:44 -0700

    Started making routes, built a movie card component

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -2,29 +2,34 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
   styleUrls: ['./user-login-form.component.scss']
 })
-export class UserLoginFormComponent {
+export class UserLoginFormComponent implements OnInit {
   @Input() userData = { UserName: '', Password: '' }
 
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+    private router: Router
   ) { }
 
   ngOnInit(): void { }
 
   loginUser(): void {
     this.fetchApiData.userLogin(this.userData).subscribe(result => {
+      localStorage.setItem('user', result.user.UserName)
+      localStorage.setItem('token', result.token)
       this.dialogRef.close()
       this.snackBar.open(result, 'OK', {
         duration: 2000
       })
+      this.router.navigate(['movies'])
     }, result => {
       this.snackBar.open(result, 'OK', {
         duration: 2000
---
+++ src/app/welcome-page/welcome-page.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
+import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
+import { MovieCardComponent } from '../movie-card/movie-card.component';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-welcome-page',
+  templateUrl: './welcome-page.component.html',
+  styleUrls: ['./welcome-page.component.scss']
+})
+export class WelcomePageComponent {
+  constructor(public dialog: MatDialog) { }
+
+  // This is the function that will open the dialog when the signup button is clicked
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      // Assigning the dialog a width
+      width: '280px'
+    })
+  };
+
+  // This is the function that will open the dialog when the login button is clicked
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      // Assigning the dialog a width
+      width: '280px'
+    })
+  }
+
+  // This function opens the movies dialog container
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px'
+    })
+  }
+}
