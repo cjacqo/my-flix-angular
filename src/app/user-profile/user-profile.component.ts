343193356f543bacb71f1917b2394687cc640a03
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-09-21 23:55:52 -0700

    Started to build the user-profile component

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss']
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+  user: any | undefined
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public router: Router
+  ) { }
+
+  ngOnInit(): void {
+    if (localStorage.getItem('user') && localStorage.getItem('token')) {
+      this.user = JSON.parse(localStorage.getItem('user')!)
+      console.log(this.user)
+    } else {
+      this.router.navigate(['welcome'])
+    }
+  }
 
 }
